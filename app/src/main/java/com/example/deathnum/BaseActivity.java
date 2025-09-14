d10f08465fbcf6f95e43a765dc8dc5313b10f2e1
Dima <161807494+Dimkatf@users.noreply.github.com>
2025-09-14 12:56:57 +0400

    Создана база поддержки английского языка для некоторых режимов. Плюс для активности правила

---
+++ app/src/main/java/com/example/deathnum/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.example.deathnum;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(App.updateLocale(newBase));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        App.updateLocale(this);
+    }
+}
